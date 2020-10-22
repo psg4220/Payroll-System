a4f3b8728048c10861ff17c9e2a5672da35e8e70
Justin Carl Vijar <vijar.194144@malolos.sti.edu.ph>
2020-10-22 17:21:47 +0800

    Changes in UserAdd.cs

--- PayrollSystem/User/UserAdd.Designer.cs
+++ PayrollSystem/User/UserAdd.Designer.cs
@@ -28,22 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelFNameRegister = new System.Windows.Forms.Label();
+            this.labelMNameRegister = new System.Windows.Forms.Label();
+            this.textBoxFNameRegister = new System.Windows.Forms.TextBox();
+            this.textBoxMNameRegister = new System.Windows.Forms.TextBox();
+            this.labelLNameRegister = new System.Windows.Forms.Label();
+            this.textBoxLNameRegister = new System.Windows.Forms.TextBox();
+            this.dtpBirthdayRegister = new System.Windows.Forms.DateTimePicker();
+            this.labelBirthdayRegister = new System.Windows.Forms.Label();
+            this.dtpHired = new System.Windows.Forms.DateTimePicker();
+            this.labelHireRegister = new System.Windows.Forms.Label();
+            this.labelAddressRegister = new System.Windows.Forms.Label();
+            this.textBoxAddressRegister = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // labelFNameRegister
+            // 
+            this.labelFNameRegister.AutoSize = true;
+            this.labelFNameRegister.Location = new System.Drawing.Point(29, 36);
+            this.labelFNameRegister.Name = "labelFNameRegister";
+            this.labelFNameRegister.Size = new System.Drawing.Size(91, 14);
+            this.labelFNameRegister.TabIndex = 0;
+            this.labelFNameRegister.Text = "First Name: ";
+            // 
+            // labelMNameRegister
+            // 
+            this.labelMNameRegister.AutoSize = true;
+            this.labelMNameRegister.Location = new System.Drawing.Point(21, 62);
+            this.labelMNameRegister.Name = "labelMNameRegister";
+            this.labelMNameRegister.Size = new System.Drawing.Size(98, 14);
+            this.labelMNameRegister.TabIndex = 1;
+            this.labelMNameRegister.Text = "Middle Name: ";
+            // 
+            // textBoxFNameRegister
+            // 
+            this.textBoxFNameRegister.Location = new System.Drawing.Point(126, 33);
+            this.textBoxFNameRegister.Name = "textBoxFNameRegister";
+            this.textBoxFNameRegister.Size = new System.Drawing.Size(134, 20);
+            this.textBoxFNameRegister.TabIndex = 2;
+            // 
+            // textBoxMNameRegister
+            // 
+            this.textBoxMNameRegister.Location = new System.Drawing.Point(126, 59);
+            this.textBoxMNameRegister.Name = "textBoxMNameRegister";
+            this.textBoxMNameRegister.Size = new System.Drawing.Size(134, 20);
+            this.textBoxMNameRegister.TabIndex = 3;
+            // 
+            // labelLNameRegister
+            // 
+            this.labelLNameRegister.AutoSize = true;
+            this.labelLNameRegister.Location = new System.Drawing.Point(35, 88);
+            this.labelLNameRegister.Name = "labelLNameRegister";
+            this.labelLNameRegister.Size = new System.Drawing.Size(84, 14);
+            this.labelLNameRegister.TabIndex = 4;
+            this.labelLNameRegister.Text = "Last Name: ";
+            // 
+            // textBoxLNameRegister
+            // 
+            this.textBoxLNameRegister.Location = new System.Drawing.Point(126, 85);
+            this.textBoxLNameRegister.Name = "textBoxLNameRegister";
+            this.textBoxLNameRegister.Size = new System.Drawing.Size(134, 20);
+            this.textBoxLNameRegister.TabIndex = 5;
+            // 
+            // dtpBirthdayRegister
+            // 
+            this.dtpBirthdayRegister.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirthdayRegister.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBirthdayRegister.Location = new System.Drawing.Point(126, 111);
+            this.dtpBirthdayRegister.Name = "dtpBirthdayRegister";
+            this.dtpBirthdayRegister.Size = new System.Drawing.Size(134, 22);
+            this.dtpBirthdayRegister.TabIndex = 6;
+            // 
+            // labelBirthdayRegister
+            // 
+            this.labelBirthdayRegister.AutoSize = true;
+            this.labelBirthdayRegister.Location = new System.Drawing.Point(12, 117);
+            this.labelBirthdayRegister.Name = "labelBirthdayRegister";
+            this.labelBirthdayRegister.Size = new System.Drawing.Size(112, 14);
+            this.labelBirthdayRegister.TabIndex = 7;
+            this.labelBirthdayRegister.Text = "Date Of Birth: ";
+            // 
+            // dtpHired
+            // 
+            this.dtpHired.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpHired.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpHired.Location = new System.Drawing.Point(126, 139);
+            this.dtpHired.Name = "dtpHired";
+            this.dtpHired.Size = new System.Drawing.Size(134, 22);
+            this.dtpHired.TabIndex = 8;
+            // 
+            // labelHireRegister
+            // 
+            this.labelHireRegister.AutoSize = true;
+            this.labelHireRegister.Location = new System.Drawing.Point(5, 145);
+            this.labelHireRegister.Name = "labelHireRegister";
+            this.labelHireRegister.Size = new System.Drawing.Size(119, 14);
+            this.labelHireRegister.TabIndex = 9;
+            this.labelHireRegister.Text = "Job Started at: ";
+            // 
+            // labelAddressRegister
+            // 
+            this.labelAddressRegister.AutoSize = true;
+            this.labelAddressRegister.Location = new System.Drawing.Point(35, 170);
+            this.labelAddressRegister.Name = "labelAddressRegister";
+            this.labelAddressRegister.Size = new System.Drawing.Size(70, 14);
+            this.labelAddressRegister.TabIndex = 10;
+            this.labelAddressRegister.Text = "Address: ";
+            // 
+            // textBoxAddressRegister
+            // 
+            this.textBoxAddressRegister.Location = new System.Drawing.Point(126, 167);
+            this.textBoxAddressRegister.Multiline = true;
+            this.textBoxAddressRegister.Name = "textBoxAddressRegister";
+            this.textBoxAddressRegister.Size = new System.Drawing.Size(134, 58);
+            this.textBoxAddressRegister.TabIndex = 11;
             // 
             // UserAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.textBoxAddressRegister);
+            this.Controls.Add(this.labelAddressRegister);
+            this.Controls.Add(this.labelHireRegister);
+            this.Controls.Add(this.dtpHired);
+            this.Controls.Add(this.labelBirthdayRegister);
+            this.Controls.Add(this.dtpBirthdayRegister);
+            this.Controls.Add(this.textBoxLNameRegister);
+            this.Controls.Add(this.labelLNameRegister);
+            this.Controls.Add(this.textBoxMNameRegister);
+            this.Controls.Add(this.textBoxFNameRegister);
+            this.Controls.Add(this.labelMNameRegister);
+            this.Controls.Add(this.labelFNameRegister);
             this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "UserAdd";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "UserAdd";
+            this.Text = "Add";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelFNameRegister;
+        private System.Windows.Forms.Label labelMNameRegister;
+        private System.Windows.Forms.TextBox textBoxFNameRegister;
+        private System.Windows.Forms.TextBox textBoxMNameRegister;
+        private System.Windows.Forms.Label labelLNameRegister;
+        private System.Windows.Forms.TextBox textBoxLNameRegister;
+        private System.Windows.Forms.DateTimePicker dtpBirthdayRegister;
+        private System.Windows.Forms.Label labelBirthdayRegister;
+        private System.Windows.Forms.DateTimePicker dtpHired;
+        private System.Windows.Forms.Label labelHireRegister;
+        private System.Windows.Forms.Label labelAddressRegister;
+        private System.Windows.Forms.TextBox textBoxAddressRegister;
     }
 }
